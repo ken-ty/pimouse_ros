2f09f631be043519ffcd9a428ad70c2692ae5c8b
Ubuntu <ubuntu@ubuntu.mshome.net>
2020-01-25 01:24:20 +0900

    edit code

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,5 +1,10 @@
 #!/bin/bash -xve
 
+#pip upgrade
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+python3 get-pip.py
+
 #required packages
 pip install --upgrade pip
 pip install catkin_pkg
